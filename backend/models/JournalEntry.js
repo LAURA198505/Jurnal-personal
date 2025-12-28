71a27df89641c4f7bee0fa4b15a51d9f1a90199a
LAURA198505 <OPRESCULAURA24@STUD.ASE.RO>
2025-12-28 18:57:31 +0200

    Add JournalEntry model

--- backend/models/JournalEntry.js
+++ backend/models/JournalEntry.js
@@ -1,1 +1,24 @@
-js
+const { DataTypes } = require("sequelize");
+const db = require("./index");
+
+const JournalEntry = db.sequelize.define("JournalEntry", {
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  content: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  mood: {
+    type: DataTypes.STRING,
+  },
+  weatherTemp: {
+    type: DataTypes.FLOAT,
+  },
+  weatherDesc: {
+    type: DataTypes.STRING,
+  },
+});
+
+module.exports = JournalEntry;
